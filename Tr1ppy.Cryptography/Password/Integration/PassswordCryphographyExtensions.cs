d3bd411f7b7389b7473cab810d2b9b2ef3226491
tr1ppy9in3 <syzuki45@gmail.com>
2025-05-04 00:41:55 +0700

    Added passwrd cryptography.

---
+++ Tr1ppy.Cryptography/Password/Integration/PassswordCryphographyExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Tr1ppy.Cryptography.Password.Integration;
+
+public static class PasswordCryptographyExtensions
+{
+    public static IServiceCollection AddPasswordCryptography
+    (
+        this IServiceCollection services,
+        Action<HashPasswordSettings> configure
+    )
+    {
+        services.Configure(configure);
+        services.AddSingleton<PasswordCryptographyService>();
+        return services;
+    }
+}
---
+++ Tr1ppy.Cryptography/Password/PasswordCryptographyService.cs
@@ -0,0 +1,69 @@
+﻿using System.Security.Cryptography;
+
+namespace Tr1ppy.Cryptography.Password;
+
+public class PasswordCryptographyService(HashPasswordSettings settings)
+{
+    private readonly HashPasswordSettings _settings = settings
+        ?? throw new ArgumentNullException(nameof(settings));
+
+    public string HashPassword(string password)
+    {
+        return HashPassword(password, _settings);
+    }
+
+    public static string HashPassword(string password, HashPasswordSettings settings)
+    {
+        byte[] salt = RandomNumberGenerator.GetBytes(settings.SaltSize);
+        byte[] hash = Rfc2898DeriveBytes.Pbkdf2
+        (
+            password: password,
+            salt: salt,
+            iterations: settings.IterationsCount,
+            hashAlgorithm: settings.HashAlgorithm,
+            outputLength: settings.HashSize
+        );
+
+        return $"{Convert.ToBase64String(salt)}.{Convert.ToBase64String(hash)}";
+    }
+
+
+    public bool VerifyPassword(string password, string hashedPassword)
+    {
+        return VerifyPassword(password, hashedPassword, _settings);
+    }
+
+    public static bool VerifyPassword(string password, string hashedPassword, HashPasswordSettings settings)
+    {
+
+        string[] parts = hashedPassword.Split('.', 2);
+        if (parts.Length != 2)
+            return false;
+
+        byte[] salt = Convert.FromBase64String(parts[0]);
+        byte[] expectedHash = Convert.FromBase64String(parts[1]);
+
+        var actualHash = Rfc2898DeriveBytes.Pbkdf2
+        (
+            password: password,
+            salt: salt,
+            iterations: settings.IterationsCount,
+            hashAlgorithm: settings.HashAlgorithm,
+            outputLength: settings.HashSize
+        );
+
+        return CryptographicOperations.FixedTimeEquals(expectedHash, actualHash);
+    }
+}
+
+
+public class HashPasswordSettings
+{
+    public required HashAlgorithmName HashAlgorithm { get; set; }
+
+    public required int HashSize { get; set; }
+
+    public required int SaltSize { get; set; }
+
+    public required int IterationsCount { get; set; }
+}
