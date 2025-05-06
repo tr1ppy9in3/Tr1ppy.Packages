8fc87fc83207fd0f3363bcf04edaf999c3b552a9
tr1ppy9in3 <syzuki45@gmail.com>
2025-05-06 23:02:43 +0700

    Added own options builder for EF. Added integration with Autofac/DI

---
+++ Tr1ppy.EntityFramework.Autofac/ContainerBuilderDbContexBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tr1ppy.EntityFramework.Autofac;
+
+public static class ContainerBuilderEntityFrameworkExtensions
+{
+    public static DbContextInternalOptionsBuilderAutofac<TContext> AddDataContextTest<TContext>(this ContainerBuilder containerBuilder) 
+        where TContext : DbContext
+    {
+        return new DbContextInternalOptionsBuilderAutofac<TContext>(containerBuilder);
+    }
+}
---
+++ Tr1ppy.EntityFramework.DependencyInjection/ServiceCollectionDbContextBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+using Tr1ppy.EntityFramework.Autofac;
+
+namespace Tr1ppy.EntityFramework.DependencyInjection;
+
+public static class ContainerBuilderEntityFrameworkExtensions
+{
+    public static DbContextInternalOptionsBuilderDependyInjection<TContext> AddDataContextTest<TContext>
+    (
+        this IServiceCollection serviceCollection
+    )
+        where TContext : DbContext
+    {
+        return new DbContextInternalOptionsBuilderDependyInjection<TContext>(serviceCollection);
+    }
+}
